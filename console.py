c96a641bd8a63ee9f9caac309233f47c561ee2fd
dacoolguy1 <emmanueltemiede@gmail.com>
2023-02-11 08:09:02 -0800

    first commit

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Here we are creating a console application for the AIRbnb project
+"""
+import cmd
+class HBNBCommand(cmd.Cmd):
+    """
+    command class interprete
+    """
+     prompt = '(hbnb) '
+    def do_quit(self, line):
+        """The function or method used to quit the console"""
+        quit()
+
+    def do_EOF(self, line):
+        """The function we will used to handle end of line"""
+        print()
+        exit()
